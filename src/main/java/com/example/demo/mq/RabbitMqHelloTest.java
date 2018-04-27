2e5a8deb09c280f8f1df3d6b2e9272697aa71579
xuqq <xuqq@guahao.com>
2018-04-27 11:13:21 +0800

    报错修改

--- src/main/java/com/example/demo/mq/RabbitMqHelloTest.java
+++ src/main/java/com/example/demo/mq/RabbitMqHelloTest.java
@@ -3,8 +3,6 @@ package com.example.demo.mq;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-import org.springframework.amqp.core.BindingBuilder;
-
 public class RabbitMqHelloTest {
 	private static final String HOST = "XXXX";
 
@@ -12,6 +10,6 @@ public class RabbitMqHelloTest {
 	private static final String PASSWORD = "xxx";
 
 	public static void main(String[] args) throws IOException, TimeoutException {
-		BindingBuilder.bind();
+
 	}
 }
