cd093c4679f6e998424e3b8c314cde2dde4f68bb
xuqq <xuqq@guahao.com>
2018-05-15 10:09:17 +0800

    queue

--- src/main/java/com/example/demo/delay/OrderDelayRunnable.java
+++ src/main/java/com/example/demo/delay/OrderDelayRunnable.java
@@ -1,34 +1,35 @@
-package com.qq.xu.test.test1;
+package com.example.demo.delay;
 
 import java.util.concurrent.DelayQueue;
 
 public class OrderDelayRunnable implements Runnable {
-    private DelayQueue<OrderDelayTask> tasks;
-    public OrderDelayRunnable(DelayQueue<OrderDelayTask> tasks) {
-    	this.tasks=tasks;
-    }
+	private DelayQueue<OrderDelayTask> tasks;
 
-    @Override
-    public void run() {
-        while (!Thread.interrupted()) {
-            try {
-                OrderDelayTask task = tasks.take();
-                if (null == task) {
-                    continue;
-                }
-                handler(task);
-            } catch (InterruptedException e) {
-            	System.out.println(e);
-                
-            }
-            System.out.println("Finished PersonalCussumer");
-        }
+	public OrderDelayRunnable(DelayQueue<OrderDelayTask> tasks) {
+		this.tasks = tasks;
+	}
 
-    }
+	@Override
+	public void run() {
+		while (!Thread.interrupted()) {
+			try {
+				OrderDelayTask task = tasks.take();
+				if (null == task) {
+					continue;
+				}
+				handler(task);
+			} catch (InterruptedException e) {
+				System.out.println(e);
 
-    private void handler(OrderDelayTask task) {
-        task.setOrderSatus("1");
-        System.out.println("order status 1");
+			}
+			System.out.println("Finished PersonalCussumer");
+		}
 
-    }
+	}
+
+	private void handler(OrderDelayTask task) {
+		task.setOrderSatus("1");
+		System.out.println("order status 1");
+
+	}
 }
--- src/main/java/com/example/demo/delay/OrderDelayTask.java
+++ src/main/java/com/example/demo/delay/OrderDelayTask.java
@@ -1,34 +1,31 @@
-package com.qq.xu.test.test1;
+package com.example.demo.delay;
 
-import java.util.Date;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 public class OrderDelayTask implements Delayed {
-    private String orderNo;
-    private String orderSatus;
-    private long executeTime;
-    private long createTime;
+	private String orderNo;
+	private String orderSatus;
+	private long executeTime;
+	private long createTime;
 
-    public String getOrderNo() {
-        return orderNo;
-    }
+	public String getOrderNo() {
+		return orderNo;
+	}
 
-    public void setOrderNo(String orderNo) {
-        this.orderNo = orderNo;
-    }
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
 
-    public String getOrderSatus() {
-        return orderSatus;
-    }
+	public String getOrderSatus() {
+		return orderSatus;
+	}
 
-    public void setOrderSatus(String orderSatus) {
-        this.orderSatus = orderSatus;
-    }
+	public void setOrderSatus(String orderSatus) {
+		this.orderSatus = orderSatus;
+	}
 
-   
-
-    public long getExecuteTime() {
+	public long getExecuteTime() {
 		return executeTime;
 	}
 
@@ -37,29 +34,30 @@ public class OrderDelayTask implements Delayed {
 	}
 
 	public long getCreateTime() {
-        return createTime;
-    }
+		return createTime;
+	}
 
-    public void setCreateTime(long createTime) {
-        this.createTime = createTime;
-    }
+	public void setCreateTime(long createTime) {
+		this.createTime = createTime;
+	}
 
-    @Override
-    public int compareTo(Delayed o) {
-    	OrderDelayTask task=(OrderDelayTask) o;
-    	if(this.createTime<task.createTime) {
-    		return 1;
-    	}else if(this.createTime==task.createTime) {
-    		return 0;
-    	}else {
-    		return -1;
-    	}
-    }
+	@Override
+	public int compareTo(Delayed o) {
+		OrderDelayTask task = (OrderDelayTask) o;
+		if (this.createTime < task.createTime) {
+			return 1;
+		} else if (this.createTime == task.createTime) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
 
-    @Override
-    public long getDelay(TimeUnit unit) {
-    	return unit.convert(this.executeTime-System.currentTimeMillis(), TimeUnit.MILLISECONDS);
-        //return createTime.getTime() + 30;
-    }
+	@Override
+	public long getDelay(TimeUnit unit) {
+		return unit.convert(this.executeTime - System.currentTimeMillis(),
+				TimeUnit.MILLISECONDS);
+		// return createTime.getTime() + 30;
+	}
 
 }
